16db63586598e60d2735e299614b5dd3f290e245
Negative_light <53065989+Negative-light@users.noreply.github.com>
2023-10-28 22:18:26 -0400

    Removed Excess info from the Contributors page to push to main

--- src/app/contributors/page.tsx
+++ src/app/contributors/page.tsx
@@ -2,35 +2,9 @@ import axios from 'axios';
 import { debug } from 'console';
 import React, { useEffect, useState } from 'react';
 
-async function getContributors(owner: string, repoName:string, page = 1) {
-    let request = await fetch('https://api.github.com/repos/${owner}/${repoName}/contributors?per_page=100&page=${page}', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        }
-    });
-    
-    let contributorsList = await request.json();
-    return contributorsList;
-}
 
-
-async function getAlllContributors(owner: string, repoName: string) {
-    let contributors = [];
-    let page = 1;
-    do {
-        list = await getContributors(owner, repoName, page);
-        contributors = contributors.concat(list);
-        page++;
-    } while (list.length > 0);
-    
-    return contributors;
-    
-}
 
 async function Home() {
-    const contributors = await getContributors('negative-light-media', 'www.negative-light.com')
-    debug(contributors)
     return <head>
         <meta charSet="UTF-8"/>
         <link rel="icon" type="image/x-icon" href="img/favicon.png"/>
